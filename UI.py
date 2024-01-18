857f16b4cedf51a4ea037752273fd7ac2e933ff9
M.Furkan İNCE <mfrknince@gmail.com>
2024-01-19 00:33:10 +0300

    UI V2.1, demo srt

--- UI.py
+++ UI.py
@@ -4,6 +4,7 @@ from WebScrapping import find_anime_picture, load_image
 from mini_translator import MiniTranslator
 from Translator import Translator
 
+st.set_page_config(layout="wide")
 st.header("Anime Altyazı Çevirici")
 
 col1, col2 = st.columns(2)
@@ -14,7 +15,7 @@ with col1:
 with col2:
     if uploaded_file is not None:
         image_url = find_anime_picture(uploaded_file.name.split(".")[0])
-        st.image(load_image(image_url), caption="Image from URL")
+        st.image(load_image(image_url), caption=uploaded_file.name.split(".")[0])
 
 if st.button("Çevir ve İşle"):
     if uploaded_file is not None:
@@ -36,3 +37,16 @@ if st.button("Çevir ve İşle"):
             file_name="cevrilen.srt",
             mime="text/plain"
         )
+st.write("\n" * 5)
+
+col1, col2, col3 = st.columns(3)
+
+
+with col1:
+    st.markdown("<h6 style='text-align: center;'>200601037 - Dinçer Kaan Turanlı</h6>", unsafe_allow_html=True)
+
+with col2:
+    st.markdown("<h6 style='text-align: center;'>200601057 - Ragıp Kaan Söylev</h6>", unsafe_allow_html=True)
+
+with col3:
+    st.markdown("<h6 style='text-align: center;'>200601059 - Muhammet Furkan İnce</h6>", unsafe_allow_html=True)
